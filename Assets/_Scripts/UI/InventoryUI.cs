da2d32de048dab9043c92d05b4d11e238e8ea4d1
RavenBP <ravenbpowless@gmail.com>
2021-02-04 15:19:46 -0500

    Added simple Inventory UI

---
+++ Assets/_Scripts/UI/InventoryUI.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryUI : MonoBehaviour
+{
+    [SerializeField]
+    GameObject towerPanel;
+    [SerializeField]
+    GameObject seedsPanel;
+    [SerializeField]
+    GameObject miscPanel;
+
+    //private GameObject currentPanel;
+
+    private void Start()
+    {
+        // Open Tower Panel by Default
+        towerPanel.SetActive(true);
+    }
+
+    public void OpenTowerPanelButton()
+    {
+        seedsPanel.SetActive(false);
+        miscPanel.SetActive(false);
+        towerPanel.SetActive(true);
+    }
+
+    public void OpenSeedPanelButton()
+    {
+        towerPanel.SetActive(false);
+        miscPanel.SetActive(false);
+        seedsPanel.SetActive(true);
+    }
+
+    public void OpenMiscPanelButton()
+    {
+        towerPanel.SetActive(false);
+        seedsPanel.SetActive(false);
+        miscPanel.SetActive(true);
+    }
+
+    public void CloseInventoryButton()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
