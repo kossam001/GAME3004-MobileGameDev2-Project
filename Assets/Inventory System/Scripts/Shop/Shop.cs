30d361ccc1f7a06ddfeffbd58dfdd61990728383
kossam001 <56322486+kossam001@users.noreply.github.com>
2021-03-07 21:44:32 -0500

    Generalized Shop code
    - Easier to scale if new shop inventories needed
    - Gave seeds plant objects to spawn just to work

--- Assets/Inventory System/Scripts/Shop/Shop.cs
+++ Assets/Inventory System/Scripts/Shop/Shop.cs
@@ -9,13 +9,17 @@ public class Shop : MonoBehaviour
     [SerializeField]
     protected ItemTable masterItemTable;
 
-    [Header("Tower Shop")]
-    [SerializeField] private GameObject towerInventoryPanel;
-    [SerializeField] protected List<ShopSlot> towerItemSlots;
+    [Header("Shop")]
+    [Tooltip("Inventory panels")]
+    [SerializeField] private List<GameObject> inventoryPanel;
+    [Tooltip("Item slot template")]
+    [SerializeField] protected ShopSlot itemTemplate;
+    [Tooltip("Inventory type")]
+    [SerializeField] protected List<ItemType> inventoryType;
 
-    [Header("Seed Shop")]
-    [SerializeField] private GameObject seedInventoryPanel;
-    [SerializeField] protected List<ShopSlot> seedItemSlots;
+    private Dictionary<ItemType, GameObject> itemTypeToInventoryTable;
+    private Dictionary<ItemType, List<ShopSlot>> itemTypeToSlotTable;
+    private List<List<ShopSlot>> itemSlots;
 
     // Start is called before the first frame update
     void Start()
@@ -25,44 +29,64 @@ public class Shop : MonoBehaviour
 
     private void InitShop()
     {
+        itemTypeToInventoryTable = new Dictionary<ItemType, GameObject>();
+        itemTypeToSlotTable = new Dictionary<ItemType, List<ShopSlot>>();
+
+        for (int i = 0; i < inventoryType.Count; i++)
+        {
+            itemTypeToInventoryTable[inventoryType[i]] = inventoryPanel[i];
+            itemTypeToSlotTable[inventoryType[i]] = new List<ShopSlot>();
+        }
+
         int seedCount = 0;
         int towerCount = 0;
 
         // Sort the items into seeds and towers and organize them into the correct inventories
         foreach (Item item in masterItemTable.items)
         {
-            switch (item.Type)
-            {
-                case ItemType.SEED:
-                    seedCount++;
-                    SetupSlot(item, seedInventoryPanel, seedItemSlots, seedCount);
-                    break;
-                case ItemType.TOWER:
-                    towerCount++;
-                    SetupSlot(item, towerInventoryPanel, towerItemSlots, towerCount);
-                    break;
-            }
+            SetupSlot(item, itemTypeToInventoryTable[item.Type], itemTypeToSlotTable[item.Type]);
+
+            //switch (item.Type)
+            //{
+            //    case ItemType.SEED:
+            //        seedCount++;
+            //        SetupSlot(item, itemTypeToInventoryTable[ItemType.SEED], itemTypeToSlotTable[ItemType.SEED], seedCount);
+            //        break;
+            //    case ItemType.TOWER:
+            //        towerCount++;
+            //        SetupSlot(item, itemTypeToInventoryTable[ItemType.TOWER], itemTypeToSlotTable[ItemType.TOWER], towerCount);
+            //        break;
+            //}
         }
     }
 
-    private void SetupSlot(Item item, GameObject parentPanel, List<ShopSlot> slots, int slotCount)
+    private void SetupSlot(Item item, GameObject parentPanel, List<ShopSlot> slots)
     {
-        ShopSlot slot;
-
-        // Generate more slots as needed
-        if (slotCount > slots.Count)
-        {
-            GameObject newObject = Instantiate(slots[0].gameObject, parentPanel.transform);
-            slot = newObject.GetComponent<ShopSlot>();
-            slots.Add(slot);
-        }
-        else
-        {
-            slot = slots[slotCount - 1];
-        }
+        GameObject newObject = Instantiate(itemTemplate.gameObject, parentPanel.transform);
+        ShopSlot slot = newObject.GetComponent<ShopSlot>();
+        slot.shop = this;
+        slots.Add(slot);
 
         slot.shop = this;
         slot.SetContents(item, 1);
+
+        //ShopSlot slot;
+
+        //// Generate more slots as needed
+        //if (slotCount > slots.Count)
+        //{
+        //    GameObject newObject = Instantiate(itemTemplate.gameObject, parentPanel.transform);
+        //    slot = newObject.GetComponent<ShopSlot>();
+        //    slot.shop = this;
+        //    slots.Add(slot);
+        //}
+        //else
+        //{
+        //    slot = slots[slotCount - 1];
+        //}
+
+        //slot.shop = this;
+        //slot.SetContents(item, 1);
     }
 
     public void SellItem(Item item)
