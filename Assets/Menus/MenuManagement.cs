bbcd2639c446a80d162e64d8e12c4a1340e89383
kossam001 <56322486+kossam001@users.noreply.github.com>
2021-04-02 13:53:30 -0400

    Added ability to click towers

--- Assets/Menus/MenuManagement.cs
+++ Assets/Menus/MenuManagement.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class MenuManagement : MonoBehaviour
 {
+    private static MenuManagement instance;
+    public static MenuManagement Instance { get { return instance; } }
+
     private GameObject activeMenu;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
 
     public void SetActiveMenu(GameObject menu)
     {
-        
         if (activeMenu)
             activeMenu.SetActive(false);
 
---
+++ Assets/Menus/TowerManagement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class TowerManagement : MonoBehaviour
+{
+    private static TowerManagement instance;
+    public static TowerManagement Instance { get { return instance; } }
+
+    public GameObject towerMenu;
+    public LayerMask rayLayer;
+
+    private bool clicked = false;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check if the left Mouse button is clicked
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !Pause.gameIsPaused)
+        {
+            Click();
+        }
+    }
+
+    private void Click()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1000, rayLayer))
+        {
+            MenuManagement.Instance.SetActiveMenu(towerMenu);
+        }
+    }
+}
