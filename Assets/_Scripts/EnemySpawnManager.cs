dd096696e76081aca844f1b917489020e17ba233
milandro <mj.jr2325@gmail.com>
2021-04-06 00:21:29 -0400

    wave system implemented

--- Assets/_Scripts/EnemySpawnManager.cs
+++ Assets/_Scripts/EnemySpawnManager.cs
@@ -7,27 +7,12 @@ using TMPro;
 public class EnemySpawnManager : MonoBehaviour
 {
     public TMP_Text waveText;
-    private int waveCounter;
+    private int waveCounter = 0;
 
     // Update is called once per frame
     void Update()
     {     
-        // Spawns Enemy (Game View Only since spacebar)
-        if (Input.GetKeyDown(KeyCode.Space) && !Pause.gameIsPaused)
-        {
-            //Debug.Log("SpawnManager Heard Space");
 
-            // Instantiate enemy prefab between the two points
-            GameObject enemy = ObjectPooling.SharedInstance.GetPooledObject("Enemy");
-            if (enemy != null)
-            {
-                enemy.transform.position = transform.position;
-                enemy.transform.rotation = transform.rotation;
-                enemy.SetActive(true);
-
-                enemy.GetComponent<NavMeshAgent>().speed = enemy.GetComponent<EnemyBehaviour>().defaultSpeed;
-            }
-        }
     }
 
     // For Spawn Enemy button
@@ -35,8 +20,16 @@ public class EnemySpawnManager : MonoBehaviour
     {
         if (!Pause.gameIsPaused)
         {
-            //waveCounter++;
-            //waveText.text = "Wave: " + waveCounter;
+            StartCoroutine(SpawnWave());
+            waveText.text = "Wave: " + waveCounter;
+         
+        }
+    }
+
+    IEnumerator SpawnWave()
+    {
+        for (int i = 0; i < waveCounter; i++)
+        {
             GameObject enemy = ObjectPooling.SharedInstance.GetPooledObject("Enemy");
             if (enemy != null)
             {
@@ -44,6 +37,8 @@ public class EnemySpawnManager : MonoBehaviour
                 enemy.transform.rotation = transform.rotation;
                 enemy.SetActive(true);
             }
+            yield return new WaitForSeconds(1.5f);
         }
+        waveCounter++; 
     }
 }
